1aac09055ad8ecf09d2de93c285daa399480d492
ChuanLiu0316 <liuchuan95@gmail.com>
2013-12-29 20:37:16 +0800

    Create Subject.m

---
+++ Model/Subject.m
@@ -0,0 +1,18 @@
+//
+//  Subject.m
+//  Mistake Notebook
+//
+//  Created by ChuanLiu on 10/24/13.
+//  Copyright (c) 2013 ChuanLiu. All rights reserved.
+//
+
+#import "Subject.h"
+#import "Mistake.h"
+
+
+@implementation Subject
+
+@dynamic title;
+@dynamic mistakes;
+
+@end
