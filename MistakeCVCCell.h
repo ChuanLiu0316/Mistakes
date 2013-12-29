44dbe75634c76becffee72a76e6a651d28460d68
ChuanLiu0316 <liuchuan95@gmail.com>
2013-12-29 20:21:36 +0800

    Create MistakeCVCCell.h

---
+++ MistakeCVCCell.h
@@ -0,0 +1,15 @@
+//
+//  MistakeCVCell.h
+//  Mistake Notebook
+//
+//  Created by ChuanLiu on 10/20/13.
+//  Copyright (c) 2013 ChuanLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MistakeView.h"
+
+@interface MistakeCVCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet MistakeView *mistakeView;
+
+@end
