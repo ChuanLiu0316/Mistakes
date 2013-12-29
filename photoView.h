6d07f512471167700a824f410c39bba2ed46fdf5
ChuanLiu0316 <liuchuan95@gmail.com>
2013-12-29 20:04:08 +0800

    Create photoView.h

---
+++ photoView.h
@@ -0,0 +1,16 @@
+//
+//  photoView.h
+//  Mistake Notebook
+//
+//  Created by ChuanLiu on 10/22/13.
+//  Copyright (c) 2013 ChuanLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Mistake.h"
+
+@interface photoView : UIView
+
+@property (nonatomic,strong) UIImage *image;
+
+@end
