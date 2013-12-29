687b17a78ceb6ad1c322d9d59234bed87b3326a7
ChuanLiu0316 <liuchuan95@gmail.com>
2013-12-29 20:27:12 +0800

    Create MistakesCVC.h

---
+++ MistakesCVC.h
@@ -0,0 +1,14 @@
+//
+//  MistakesCVC.h
+//  Mistake Notebook
+//
+//  Created by ChuanLiu on 10/20/13.
+//  Copyright (c) 2013 ChuanLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Subject.h"
+
+@interface MistakesCVC : UIViewController
+@property (nonatomic,strong) Subject *subject;
+@end
