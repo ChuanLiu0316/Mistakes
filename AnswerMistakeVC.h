0f2a4730199d18549e291033e77d3aaee522269e
ChuanLiu0316 <liuchuan95@gmail.com>
2013-12-29 20:17:26 +0800

    Create AnswerMistakeVC.h

---
+++ AnswerMistakeVC.h
@@ -0,0 +1,15 @@
+//
+//  AnswerMistakeVC.h
+//  Mistake Notebook
+//
+//  Created by ChuanLiu on 10/22/13.
+//  Copyright (c) 2013 ChuanLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Mistake.h"
+@interface AnswerMistakeVC : UIViewController
+
+@property (nonatomic,strong) Mistake *mistake;
+
+@end
