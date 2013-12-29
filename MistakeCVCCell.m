82a9768219d9741b79a8b3109db563d08333ceab
ChuanLiu0316 <liuchuan95@gmail.com>
2013-12-29 20:24:55 +0800

    Create MistakeCVCCell.m

---
+++ MistakeCVCCell.m
@@ -0,0 +1,15 @@
+//
+//  MistakeCVCell.m
+//  Mistake Notebook
+//
+//  Created by ChuanLiu on 10/20/13.
+//  Copyright (c) 2013 ChuanLiu. All rights reserved.
+//
+
+#import "MistakeCVCell.h"
+
+@implementation MistakeCVCell
+
+
+
+@end
