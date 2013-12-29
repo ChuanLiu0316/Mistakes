cb20a8d452d9eb2168c2b06d5bf3d03c6bc676bd
ChuanLiu0316 <liuchuan95@gmail.com>
2013-12-29 20:11:17 +0800

    Create CreateMistakeVC.h

---
+++ CreateMistakeVC.h
@@ -0,0 +1,18 @@
+//
+//  CreateMistakeVC.h
+//  Mistake Notebook
+//
+//  Created by ChuanLiu on 10/22/13.
+//  Copyright (c) 2013 ChuanLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateMistakeVC : UIViewController
+
+
+@property (nonatomic,strong) NSString *answer;
+@property (nonatomic,strong) NSURL *imageUrl;
+@property (nonatomic,strong) UIImage *image;
+
+@end
