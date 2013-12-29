d3856b1403b0cca9bb9867dd67c75d1260dffc4a
ChuanLiu0316 <liuchuan95@gmail.com>
2013-12-29 20:38:34 +0800

    Create Mistake.m

---
+++ Model/Mistake.m
@@ -0,0 +1,20 @@
+//
+//  Mistake.m
+//  Mistake Notebook
+//
+//  Created by ChuanLiu on 10/24/13.
+//  Copyright (c) 2013 ChuanLiu. All rights reserved.
+//
+
+#import "Mistake.h"
+#import "Subject.h"
+
+
+@implementation Mistake
+
+@dynamic answer;
+@dynamic imageUrl;
+@dynamic number;
+@dynamic whichSubject;
+
+@end
