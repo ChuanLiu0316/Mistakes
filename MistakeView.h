c4c6c8accfd4f071c306428fc6d1f88378fce9de
ChuanLiu0316 <liuchuan95@gmail.com>
2013-12-29 20:25:52 +0800

    Create MistakeView.h

---
+++ MistakeView.h
@@ -0,0 +1,17 @@
+//
+//  MistakeView.h
+//  Mistake Notebook
+//
+//  Created by ChuanLiu on 10/20/13.
+//  Copyright (c) 2013 ChuanLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Mistake.h"
+
+@interface MistakeView : UIView
+@property (nonatomic,strong) Mistake *mistake;
+
+
+
+@end
