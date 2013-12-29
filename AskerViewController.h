d2bc11ba2cd7b3e41eddef0eae0e7de06bbce969
ChuanLiu0316 <liuchuan95@gmail.com>
2013-12-29 20:31:31 +0800

    Create AskerViewController.h

---
+++ AskerViewController.h
@@ -0,0 +1,20 @@
+//
+//  AskerViewController.h
+//  Mistake Notebook
+//
+//  Created by ChuanLiu on 10/19/13.
+//  Copyright (c) 2013 ChuanLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+
+
+@interface AskerViewController : UIViewController
+
+@property (nonatomic,strong) NSString *answer;
+@property (nonatomic,strong) NSString *question;
+
+@end
